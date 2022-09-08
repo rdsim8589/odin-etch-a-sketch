20826f0bbc39a48bd7e520f26f3f6cbefb118c1f
Richard Sim <rdsim8589@gmail.com>
2022-09-08 16:48:06 -0400

    Update functionality for dynamic grid resizing

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-//make 16 x 16 box
-
-// method to create 1 row of 16 boxes 
-// return the element contain 1 row of 16 boxes
-function createRow() {
+function createRow(size) {
     const row = document.createElement("div");
     row.classList.add("row");
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < size; i++) {
         const square = document.createElement("div");
         square.classList.add(`square-${i}`, "square");
         row.appendChild(square);
@@ -14,26 +10,44 @@ function createRow() {
 }
 
 // method to call  and insert into the html
-function buildGrind() {
+function buildGrid(size) {
+    if (size < 0) return;
+
     const container = document.querySelector(".container");
-    for (let i = 0; i < 16; i++) {
-        console.log(`enter ${i}`);
-        container.appendChild(createRow());
+    for (let i = 0; i < size; i++) {
+        container.appendChild(createRow(size));
     }
+    //hovereffect to change color of square
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => {
+        square.addEventListener('mouseover', changeColor);
+    })
 }
 
 function changeColor(event) {
     this.style.cssText = "background-color: aqua"
-    console.log("hello");
+}
+
+function userInput() {
+    const button = document.querySelector("[type='button']");
+    const input = document.querySelector("[type='number']");
+    button.addEventListener("click", () => {
+        // check user input
+        const gridSize = input.value;
+        console.log(gridSize);
+        //delete grid
+        const rows = document.querySelectorAll(".row");
+        rows.forEach((row) => {
+            row.remove();
+        })
+        //rebuild grid
+        buildGrid(gridSize);
+    })
 }
 
 function start() {
-    buildGrind();
-    //hovereffect to change color of square
-    const squares = document.querySelectorAll(".square");
-    squares.forEach(square =>{
-        square.addEventListener('mouseover', changeColor);
-    })
+    buildGrid(16);
+    userInput();
 }
 
 start();
